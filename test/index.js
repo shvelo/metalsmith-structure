9461a65a67b42f87715eaf4eeeca5991aef05dc1
Nick Shvelidze <nshvelidze@qarva.com>
2018-11-22 13:31:00 +0400

    Generate structure from headings

--- test/index.js
+++ test/index.js
@@ -18,34 +18,4 @@ describe('metalsmith-headings', function(){
         done();
       });
   });
-
-  it('should accept a string shorthand', function(done){
-    Metalsmith('test/fixture')
-      .use(markdown())
-      .use(headings('h2'))
-      .build(function(err, files){
-        if (err) return done(err);
-        assert.deepEqual(files['index.html'].headings, [
-          { id: 'two-one', tag: 'h2', text: 'two one' },
-          { id: 'two-two', tag: 'h2', text: 'two two' }
-        ]);
-        done();
-      });
-  });
-
-  it('should preserve order with multiple selectors', function(done){
-    Metalsmith('test/fixture')
-      .use(markdown())
-      .use(headings({ selectors: ['h1', 'h2']}))
-      .build(function(err, files){
-        if (err) return done(err);
-        assert.deepEqual(files['index.html'].headings, [
-          { id: 'one-one', tag: 'h1', text: 'one one' },
-          { id: 'two-one', tag: 'h2', text: 'two one' },
-          { id: 'one-two', tag: 'h1', text: 'one two' },
-          { id: 'two-two', tag: 'h2', text: 'two two' }
-        ]);
-        done();
-      });
-  });
 });
